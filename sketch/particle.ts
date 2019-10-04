81282ce846f73f940fd13e82d18d6e6f22d6ce58
Vincent Gschwend <vincent@crypta.li>
2019-10-04 19:23:21 +0100

    Add google lint

--- sketch/particle.ts
+++ sketch/particle.ts
@@ -1,54 +1,56 @@
 class Particle {
-    pos: p5.Vector
-    vel: p5.Vector
-    acc: p5.Vector
-    maxSpeed: number
-    color: p5.Color
-    tempColor: p5.Color
+  pos: p5.Vector;
+  vel: p5.Vector;
+  acc: p5.Vector;
+  maxSpeed: number;
+  color: p5.Color;
+  tempColor: p5.Color;
 
-    constructor() {
-        this.pos = createVector(random(width), random(height))
-        this.vel = createVector(0, 0)
-        this.acc = createVector(0, 0)
-        this.maxSpeed = 0.6
-        this.color = color(0, 0, 0)
-        this.tempColor = color(0, 0, 0)
-    }
+  constructor() {
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(0, 0);
+    this.acc = createVector(0, 0);
+    this.maxSpeed = 0.6;
+    this.color = color(0, 0, 0);
+    this.tempColor = color(0, 0, 0);
+  }
 
-    update() {
-        this.vel.add(this.acc)
-        this.vel.limit(this.maxSpeed)
-        this.pos.add(this.vel)
-        this.acc.mult(0)
-    }
+  update() {
+    this.vel.add(this.acc);
+    this.vel.limit(this.maxSpeed);
+    this.pos.add(this.vel);
+    this.acc.mult(0);
+  }
 
-    applyForce(force: p5.Vector) {
-        this.acc.add(force)
-        this.tempColor = color(0, 255, 0)
-        if (force == null) return
-        this.tempColor = (force.copy().heading() > 0) ? color(240, 3, 252, 125) : color(3, 252, 227, 125)
-    }
+  applyForce(force: p5.Vector) {
+    this.acc.add(force);
+    this.tempColor = color(0, 255, 0);
+    if (force == null) return;
+    this.tempColor =
+      force.copy().heading() > 0
+        ? color(240, 3, 252, 125)
+        : color(3, 252, 227, 125);
+  }
 
-    show() {
-        this.color = lerpColor(this.color, this.tempColor, 0.008)
-        fill(this.color)
-        noStroke()
-        circle(this.pos.x, this.pos.y, 0.5)
+  show() {
+    this.color = lerpColor(this.color, this.tempColor, 0.008);
+    fill(this.color);
+    noStroke();
+    circle(this.pos.x, this.pos.y, 0.5);
+  }
 
-    }
+  edges() {
+    if (this.pos.x > width) this.pos.x = 0;
+    if (this.pos.x < 0) this.pos.x = width;
+    if (this.pos.y > height) this.pos.y = 0;
+    if (this.pos.y < 0) this.pos.y = height;
+  }
 
-    edges() {
-        if (this.pos.x > width) this.pos.x = 0
-        if (this.pos.x < 0) this.pos.x = width
-        if (this.pos.y > height) this.pos.y = 0
-        if (this.pos.y < 0) this.pos.y = height
-    }
-
-    follow(vectors: p5.Vector[]) {
-        let x = floor(this.pos.x / scl)
-        let y = floor(this.pos.y / scl)
-        let index = x + y * cols
-        let force = vectors[index]
-        this.applyForce(force)
-    }
+  follow(vectors: p5.Vector[]) {
+    const x = floor(this.pos.x / scl);
+    const y = floor(this.pos.y / scl);
+    const index = x + y * cols;
+    const force = vectors[index];
+    this.applyForce(force);
+  }
 }
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,52 +1,51 @@
-let inc = 0.08
-let scl = 50
-let rows = 0
-let cols = 0
-let zoff = 0
-let magn = 0.005
-let zoffStep = 0.0001
+let inc = 0.08;
+let scl = 50;
+let rows = 0;
+let cols = 0;
+let zoff = 0;
+let magn = 0.005;
+let zoffStep = 0.0001;
 
-let particles: Array<Particle> = []
-let flowField: p5.Vector[]
-let particleSize: number = 50
+let particles: Particle[] = [];
+let flowField: p5.Vector[];
+let particleSize = 50;
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight, P2D)
-    background(0)
-    colorMode(RGB, 255)
-    cols = floor(innerWidth / scl)
-    rows = floor(innerHeight / scl)
-    for (let i = 0; i < particleSize; i++) {
-        particles[i] = new Particle()
-    }
-    flowField = new Array<p5.Vector>(cols * rows)
+  createCanvas(window.innerWidth, window.innerHeight, P2D);
+  background(0);
+  colorMode(RGB, 255);
+  cols = floor(innerWidth / scl);
+  rows = floor(innerHeight / scl);
+  for (let i = 0; i < particleSize; i++) {
+    particles[i] = new Particle();
+  }
+  flowField = new Array<p5.Vector>(cols * rows);
 }
 
 function draw() {
-    let yoff = 0;
-    for (let y = 0; y < rows; y++) {
-        let xoff = 0;
-        for (let x = 0; x < cols; x++) {
-            let index = x + y * cols
-            let angle = noise(xoff, yoff, zoff) * TWO_PI
-            let v = p5.Vector.fromAngle(angle)
-            v.setMag(magn)
-            flowField[index] = v
-            xoff += inc
-            push()
-            translate(x * scl, y * scl)
-            rotate(v.heading())
-            pop()
-        }
-        yoff += inc
-        zoff += zoffStep
-
-        for (let i = 0; i < particles.length; i++) {
-            particles[i].follow(flowField)
-            particles[i].update()
-            particles[i].edges()
-            particles[i].show()
-        }
+  let yoff = 0;
+  for (let y = 0; y < rows; y++) {
+    let xoff = 0;
+    for (let x = 0; x < cols; x++) {
+      const index = x + y * cols;
+      const angle = noise(xoff, yoff, zoff) * TWO_PI;
+      const v = p5.Vector.fromAngle(angle);
+      v.setMag(magn);
+      flowField[index] = v;
+      xoff += inc;
+      push();
+      translate(x * scl, y * scl);
+      rotate(v.heading());
+      pop();
     }
+    yoff += inc;
+    zoff += zoffStep;
 
+    for (let i = 0; i < particles.length; i++) {
+      particles[i].follow(flowField);
+      particles[i].update();
+      particles[i].edges();
+      particles[i].show();
+    }
+  }
 }
